f3cd06b6cfda81470affff50b9a7c988ca8c651f
Kevin See <kevin.see@biomark.com>
2019-10-23 15:17:02 -0700

    updated calc_cu_metrics.R function

--- R/calc_cu_metrics.R
+++ R/calc_cu_metrics.R
@@ -12,15 +12,40 @@
 
 calc_cu_metrics = function(folder_name = NULL) {
   
-  cu_mets = read_csv(paste(folder_name, 'CU_1.csv', sep = '/'))
+  cu_mets = read_csv(paste(folder_name, 'CU_1.csv', sep = '/')) %>%
+    select(-starts_with('Pebble'))
   
   wood_mets = read_csv(paste(folder_name, 'Wood_2.csv', sep = '/')) %>%
     woodMetrics()
   
-  read_csv(paste(folder_name, 'Jam_3.csv', sep = '/')) %>%
-    jamMetrics()
+  jam_mets = read_csv(paste(folder_name, 'Jam_3.csv', sep = '/')) %>%
+    jamMetrics() %>%
+    group_by(parentGlobalId) %>%
+    summarise_at(vars(estimatedNumberOfPieces, jamVolume),
+                 list(sum))
   
-  read_csv(paste(folder_name, 'Undercut_4.csv', sep = '/')) %>%
-    undercutMetrics()
+  und_mets = read_csv(paste(folder_name, 'Undercut_4.csv', sep = '/')) %>%
+    undercutMetrics() %>%
+    group_by(parentGlobalId) %>%
+    summarise_at(vars(undercutAT, undercutLength, nUndercuts),
+                 list(sum))
+    
+  
+  cu_mets %>%
+    select(-starts_with('Width')) %>%
+    rename(parentGlobalId = GlobalID) %>%
+    full_join(wood_mets) %>%
+    full_join(jam_mets) %>%
+    full_join(und_mets)
+  
+  
+  
+  read_csv(paste(folder_name, 'Discharge_5.csv', sep = '/')) %>%
+    as.data.frame()
+  
+  read_csv(paste(folder_name, 'DischargeMeasurements_6.csv', sep = '/'))
+  
+  cu_mets %>%
+    select(GlobalID, starts_with("Pebble"))
   
 }
