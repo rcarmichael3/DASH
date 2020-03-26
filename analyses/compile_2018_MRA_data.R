057bbba6a79122c7fdf9b5935ee60a5ffbf59b53
rcarmichael3 <richie.carmichael@biomark.com>
2020-03-26 13:11:06 -0600

    Revert "Added 'Hab_Roll' centerlines. Changed left_join code. Started working on functions for joining GAA, Nortwest, almost finished sin code."
    This reverts commit 51286da7110f6b0879d6f8d1c1f8c06c415c72fe.

--- analyses/compile_2018_MRA_data.R
+++ analyses/compile_2018_MRA_data.R
@@ -9,20 +9,18 @@
 library(tidyverse)
 library(ggplot2)
 library(sf)
-library(sp)
 library(usethis)
-library(raster)
 
 #-----------------------------------------------------------------
 # read in data
-upper_lemhi_line  = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches_RC/UL_Line_Fish.shp')
-upper_lemhi_poly  = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches_RC/UL_Poly_Fish.shp')
-lower_lemhi_line  = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches_RC/LL_Line_Fish.shp')
-lower_lemhi_poly  = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches_RC/LL_Poly_Fish.shp')
-pahs_line         = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches_RC/Pah_Line_Fish.shp')
-pahs_poly         = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches_RC/Pah_Poly_Fish.shp')
-upper_salmon_line = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches_RC/US_Line_Fish.shp')
-upper_salmon_poly = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches_RC/US_Poly_Fish.shp')
+upper_lemhi_line  = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches/UL_Line_Fish.shp')
+upper_lemhi_poly  = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches/UL_Poly_Fish.shp')
+lower_lemhi_line  = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches/LL_Line_Fish.shp')
+lower_lemhi_poly  = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches/LL_Poly_Fish.shp')
+pahs_line         = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches/Pah_Line_Fish.shp')
+pahs_poly         = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches/Pah_Poly_Fish.shp')
+upper_salmon_line = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches/US_Line_Fish.shp')
+upper_salmon_poly = st_read('C:/Git/DASH/data/raw/dash2018/ShapefilesWith_MetricsV3_reaches/US_Poly_Fish.shp')
 
 # Upper Lemhi
 ggplot() +
@@ -60,14 +58,9 @@ upper_lemhi_poly = upper_lemhi_poly %>%
 upper_lemhi = rbind(upper_lemhi_line, upper_lemhi_poly)
 upper_lemhi = as.data.frame(upper_lemhi)
 
-# remove 'Reach' from lower lemhi data
-lower_lemhi_line = lower_lemhi_line %>%
-  select(-Reach)
-
 # merge lower lemhi data
 lower_lemhi_line = lower_lemhi_line %>%
-  mutate(A_Vg_Cv = NA, Length = NA, Reach = NA) %>%
-  select(-Reach)
+  mutate(A_Vg_Cv = NA, Length = NA, Reach = NA)
 lower_lemhi_poly = lower_lemhi_poly %>%
   mutate(Avg_wdt = NA, SHAPE_L = NA, sinusty = NA)
 lower_lemhi = rbind(lower_lemhi_line, lower_lemhi_poly)
@@ -83,23 +76,22 @@ pahs = as.data.frame(pahs)
 
 # merge upper salmon data
 upper_salmon_line = upper_salmon_line %>%
-  mutate(A_Vg_Cv = NA, Length = NA, sinusty = NA) %>%
-  rename(Hab_Roll = hr)
+  mutate(A_Vg_Cv = NA, Length = NA, sinusty = NA)
 upper_salmon_poly = upper_salmon_poly %>%
-  mutate(Avg_wdt = NA, SHAPE_L = NA, sinusty = NA) %>%
-  rename(Hab_Roll = hr)
+  mutate(Avg_wdt = NA, SHAPE_L = NA, sinusty = NA)
 upper_salmon = rbind(upper_salmon_line, upper_salmon_poly)
 upper_salmon = as.data.frame(upper_salmon)
 
-
+# remove 'Reach' from lower lemhi data
+lower_lemhi = lower_lemhi %>%
+  select(-Reach)
 
 # merge all sites
 dash2018_cu = rbind(upper_lemhi, lower_lemhi, pahs, upper_salmon) %>%
   select(SiteNam, Reach_Nmb, everything()) %>%
   mutate(Len = ifelse(is.na(Length), SHAPE_L, Length)) %>%
   select(- Length, SHAPE_L) %>%
-  rename(Length = Len) %>%
-  select(-sinusty)
+  rename(Length = Len)
 
 
 # begin rolling up summaries by fish reach
@@ -143,9 +135,7 @@ Slowwater_Pct <- dash2018_cu %>%
   mutate(Slowwat_Pct = (Pool_A/Total_A))
 
 dash2018_fr = dash2018_fr %>%
-  left_join(Slowwater_Pct, by = c("SiteNam", "Reach_Nmb")) %>%
-  select(-Total_A)
-  
+  left_join(Slowwater_Pct)
 
 ##Side channel percent
 SC_A <- dash2018_cu %>%
@@ -163,7 +153,7 @@ SC_Pct <- dash2018_cu %>%
   select(-Total_A)
 
 dash2018_fr = dash2018_fr %>%
-  left_join(SC_Pct, by = c("SiteNam", "Reach_Nmb"))
+  left_join(SC_Pct)
 
 ##large wood frequency
 LW_count_wet <- dash2018_cu %>%
@@ -179,30 +169,16 @@ LWFreq_Wet <- dash2018_cu %>%
   select(-Total_L)
 
 dash2018_fr = dash2018_fr %>%
-  left_join(LWFreq_Wet, by = c("SiteNam","Reach_Nmb"))
+  left_join(LWFreq_Wet)
 
 # NatPrin1: from master sample points data
 # DistPrin1: from master sample points data
 # avg_aug_temp: from Norwest data which Richie is joining to master sample points
-
-
 # Sin_CL: calculated from imagery for each fish reach
-LL_CL_sin <- st_read("C:/GIT/DASH/data/raw/dash2018/LowerLemhi/LL_Centerline_Dissolve.shp")
-cl_length <- as.numeric(st_length((LL_CL_sin)))
-
-
-cl_points <- (LL_CL_sin) %>%
-  select("Hab_Roll") 
-cl_points <- st_cast(cl_points, "Point")
-start <- cl_points[1,] 
-start_sp <- as(start, "Spatial")
-1
-end <- cl_points[nrow(cl_points),]
-end_sp <- as(end, Class = "Spatial")
-line <- pointDistance(start, end)
-
+# WetWdth_CV: Reconsider. Can we automate calculation of this? Maybe we need to remove
 # WetBraid
 
+# 'Discharge','WetWdth_Int', 'LWVol_WetFstTurb'))
 
 # write fish reach data to csv
 write_csv(dash2018_fr, 'data/prepped/dash2018_fr.csv')
