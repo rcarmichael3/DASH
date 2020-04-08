478731a6538cef03db444d127abc1d2611f1e09d
Kevin See <Kevin.See@merck.com>
2020-04-08 12:43:12 -0700

    added code to drop one CU from upper Lemhi, and fix data from one CU in Pahsimeroi.
    tested whether 2 versions of cu_plus were identical
    
    inserted 2 additional ways to summarise hab reaches. Tested whether they were identical to original.

--- analyses/dash2018_rollup.R
+++ analyses/dash2018_rollup.R
@@ -12,8 +12,8 @@
 # load necessary libraries
 #-----------------------------
 library(sf)
-library(ggplot2)
 library(tidyverse)
+library(magrittr)
 
 #-----------------------------
 # read in 2018 MRA data stored as shapefiles
@@ -96,6 +96,26 @@ ph_sf = ph_sf %>%
                  Sgmnt_N = NA,
                  Hab_Roll = replace_na(Hab_Roll, 5)) )
 
+#-----------------------------
+# fix a couple channel units with wrong segment number
+# looks like these channel units did not have Survey123 data for some reason
+#-----------------------------
+# upper Lemhi unit 151 should be dropped
+ul_sf %<>%
+  filter(Sgmnt_N != 0)
+
+ph_sf <- ph_sf %>%
+  filter(Sgmnt_N != 0) %>%
+  rbind(ph_sf %>%
+          filter(Sgmnt_N == 0) %>%
+          mutate(Sgmnt_N = 1,
+                 Unt_Typ = 'Pool',
+                 Prc_Fns = 90,
+                 Prc_Grv = 10,
+                 Mx_Dpth = 1.32) %>%
+          mutate_at(vars(starts_with('Undrc_')),
+                    list(~ as.numeric(0)))) %>%
+  arrange(Sgmnt_N, Unt_Nmb)
 
 #-----------------------------
 # merge data together and do some cleaning
@@ -105,6 +125,7 @@ dash2018_cu = rbind(ul_sf, ll_sf, ph_sf, us_sf) %>%
   group_by(SiteNam) %>%
   ungroup() %>%
   mutate(Length = ifelse(is.na(Length), SHAPE_L, Length)) %>%
+  mutate(Tot_Cov = 100 - No_Cov) %>%
   select(-SHAPE_L, -sinusty, -Nhat, - Density) %>%
   rename(Sgmnt_ID = Sgmnt_N,
          CU_ID = Unt_Nmb,
@@ -118,7 +139,7 @@ dash2018_cu = rbind(ul_sf, ll_sf, ph_sf, us_sf) %>%
   select(SiteNam, Sgmnt_ID, CU_ID, Hab_Rch, 
          Fish_Rch, CU_Typ, Off_Chnl_Typ, Glbl_ID,            # site & unit info
          Length, SHAPE_A, Mx_Dpth, Avg_Wdth,                 # size
-         No_Cov, Aq_Veg_Cov,                                  # cover
+         Tot_Cov, No_Cov, Aq_Veg_Cov,                                  # cover
          Wod_Cnt, N_Bllst, N_ChFrm, N_Wet, Wod_Vlm, Jam_Vlm, # wood
          Undrc_C, Undrc_L, Undrc_A,                          # undercut
          d50, d84, Prc_Fns, Prc_Grv, Prc_Cbb, Prc_Bld,       # substrate
@@ -156,15 +177,16 @@ dash2018_cu = rbind(ul_sf, ll_sf, ph_sf, us_sf) %>%
 
 # any channel units missing a CU_Typ?
 dash2018_cu %>%
-  filter(is.na(CU_Typ))
+  filter(is.na(CU_Typ)) %>%
+  st_drop_geometry() %>%
+  as.data.frame()
 
 # which channel units have segment ID == 00?
 dash2018_cu %>%
   filter(Sgmnt_ID == "00")
 
-
 # xtabs(~ CU_Typ, dash2018_cu)
-# xtabs(~ CU_Typ + Off_Chnl_Typ, dash2018_cu)
+xtabs(~ CU_Typ + is.na(Off_Chnl_Typ), dash2018_cu)
 
 # clean up some datasets
 # rm(ul_sf, ll_sf, ph_sf, us_sf)
@@ -231,6 +253,20 @@ dash2018_cu_plus2 = dash2018_cu %>%
   mutate_at(vars(starts_with("hr_")),
             ~replace(., . %in% c("NaN", "-Inf"), NA))
 
+# are these 2 data frames identical?
+dash2018_cu_plus2 %>%
+  select(one_of(names(dash2018_cu_plus))) %>%
+  mutate_at(vars(which(sapply(., FUN = function(x) class(x)[1]) %in% c('numeric', 'integer'))),
+            list(round),
+            digits = 10) %>%
+  arrange(SiteNam, Hab_Rch, CU_ID) %>%
+  identical(dash2018_cu_plus %>%
+              mutate_at(vars(which(sapply(., FUN = function(x) class(x)[1]) %in% c('numeric', 'integer'))),
+                        list(round),
+                        digits = 10) %>%
+              arrange(SiteNam, Hab_Rch, CU_ID))
+# yes they are
+
 
 # save channel unit scale data
 save(dash2018_cu, dash2018_cu_plus, file = "data/prepped/dash2018_cus.Rda")
@@ -246,29 +282,27 @@ save(dash2018_cu, dash2018_cu_plus, file = "data/prepped/dash2018_cus.Rda")
 #-----------------------------
 # plot dash2018_cu for visualization
 #-----------------------------
-dash_cu_plotlist = purrr::map(unique(dash2018_cu_plus$SiteNam),
-                              function (x) {
-                                
-                                # subset data
-                                temp_sf = subset(dash2018_cu_plus, SiteNam == x)
-                                
-                                ggplot() +
-                                  geom_sf(data = temp_sf, aes(fill = CU_Typ)) +
-                                  guides(fill = FALSE) +
-                                  ggtitle(x) +
-                                  theme_bw()
-                              })
+dash_cu_plotlist = dash2018_cu_plus %>%
+  mutate(SiteName = SiteNam) %>%
+  group_by(SiteNam) %>%
+  nest() %>%
+  mutate(fig = map(data,
+                   .f = function(x) {
+                     x %>%
+                       ggplot() +
+                       geom_sf(aes(fill = CU_Typ)) +
+                       guides(fill = FALSE) +
+                       ggtitle(unique(x$SiteName)) +
+                       theme_bw()
+                   }))
 
-dash_cu_plot = cowplot::plot_grid(plotlist = dash_cu_plotlist)
+dash_cu_plot = cowplot::plot_grid(plotlist = dash_cu_plotlist$fig)
 dash_cu_plot
 
 #-----------------------------
 # begin to summarize data using Hab_Rch column
 #--------------------------
-dash2018_hr = dash2018_cu %>%
-  group_by(SiteNam, Hab_Rch) %>%
-
-
+# original calculations
 dash2018_hr = dash2018_cu_plus %>%
   group_by(SiteNam, Hab_Rch) %>%
   summarise(CU_IDs = unique(hr_CU_IDs),
@@ -305,11 +339,164 @@ dash2018_hr = dash2018_cu_plus %>%
             Prc_Bld = unique(hr_Prc_Bld),
             Pool_A = unique(hr_Pool_A),
             SC_A = unique(hr_SC_A)) %>%
+  ungroup() %>%
   mutate(FstNT_Cnt = Run_Cnt,
          FstTurb_Cnt = Riffle_Cnt + Rapid_Cnt,
          Slowwater_Pct = (Pool_A / SHAPE_A) * 100,
          SC_Pct = (SC_A / SHAPE_A) * 100,
-         LWFreq_Wet = (N_Wet / Length) * 100)
+         LWFreq_Wet = (N_Wet / Length) * 100) %>%
+  mutate_at(vars(-SiteNam, -Hab_Rch),
+            ~replace(., . %in% c("NaN", "-Inf"), NA))
+
+# calculating all metrics directly from cu, not cu_plus
+dash2018_hr2 = dash2018_cu %>%
+  group_by(SiteNam, Hab_Rch) %>%
+  summarise(CU_IDs = list(unique(CU_ID)),
+            Sgmnt_Cnt = n_distinct(Sgmnt_ID),
+            CU_Cnt = n_distinct(CU_ID),
+            Pool_Cnt = sum(CU_Typ == "Pool"),
+            Run_Cnt = sum(CU_Typ == "Run"),
+            Riffle_Cnt = sum(CU_Typ == "Riffle"),
+            Rapid_Cnt = sum(CU_Typ == "Rapid"),
+            Lsc_Cnt = sum(CU_Typ == "Lsc"),
+            Ssc_Cnt = sum(CU_Typ == "Ssc"),
+            Oca_Cnt = sum(CU_Typ == "Oca"),
+            Length = sum(Length),
+            SHAPE_A = sum(SHAPE_A),
+            Wod_Cnt = sum(Wod_Cnt),
+            N_Bllst = sum(N_Bllst),
+            N_ChFrm = sum(N_ChFrm),
+            N_Wet = sum(N_Wet),
+            Wod_Vlm = sum(Wod_Vlm),
+            Jam_Vlm = sum(Jam_Vlm),
+            Undrc_C = sum(Undrc_C),
+            Undrc_L = sum(Undrc_L),
+            Undrc_A = sum(Undrc_A),
+            Pool_Mx_Dpth = max(Mx_Dpth, na.rm = T),
+            Pool_Avg_Mx_Dpth = mean(Mx_Dpth, na.rm = T),
+            Pool_CV_Mx_Dpth = sd(Mx_Dpth, na.rm = T) / Pool_Avg_Mx_Dpth) %>%
+  left_join(dash2018_cu %>%
+              st_drop_geometry() %>%
+              group_by(SiteNam, Hab_Rch) %>%
+              summarise(Tot_Cov = weighted.mean(Tot_Cov, SHAPE_A),
+                        Aq_Veg_Cov = weighted.mean(Aq_Veg_Cov, SHAPE_A),
+                        d50 = weighted.mean(d50, SHAPE_A, na.rm = T),
+                        d84 = weighted.mean(d84, SHAPE_A, na.rm = T),
+                        Prc_Fns = weighted.mean(Prc_Fns, SHAPE_A, na.rm = T),
+                        Prc_Grv = weighted.mean(Prc_Grv, SHAPE_A, na.rm = T),
+                        Prc_Cbb = weighted.mean(Prc_Cbb, SHAPE_A, na.rm = T),
+                        Prc_Bld = weighted.mean(Prc_Bld, SHAPE_A, na.rm = T),
+                        Pool_A = sum(SHAPE_A[CU_Typ == "Pool"]),
+                        SC_A = sum(SHAPE_A[Sgmnt_ID > 1])) %>%
+              mutate_at(vars(ends_with("Cov")),
+                        list(round),
+                        digits = 2)) %>%
+  ungroup() %>%
+  mutate(FstNT_Cnt = Run_Cnt,
+         FstTurb_Cnt = Riffle_Cnt + Rapid_Cnt,
+         Slowwater_Pct = (Pool_A / SHAPE_A) * 100,
+         SC_Pct = (SC_A / SHAPE_A) * 100,
+         LWFreq_Wet = (N_Wet / Length) * 100) %>%
+  mutate_at(vars(-SiteNam, -Hab_Rch),
+            ~replace(., . %in% c("NaN", "-Inf"), NA))
+
+# another way, combining some of similarly calculated metrics
+dash2018_hr3 = dash2018_cu %>%
+  group_by(SiteNam, Hab_Rch) %>%
+  summarise(CU_IDs = list(CU_ID),
+            Sgmnt_Cnt = n_distinct(Sgmnt_ID),
+            CU_Cnt = n_distinct(CU_ID)) %>%
+  left_join(dash2018_cu %>%
+              st_drop_geometry() %>%
+              mutate(CU_Typ = fct_relevel(CU_Typ,
+                                          "Pool", "Run", 'Riffle',
+                                          'Rapid', 'Lsc', 'Ssc', 'Oca')) %>%
+              mutate(CU_Typ = fct_explicit_na(CU_Typ,
+                                              "NA")) %>%
+              group_by(SiteNam, Hab_Rch, CU_Typ) %>%
+              summarise(cnt = n_distinct(CU_ID)) %>%
+              ungroup() %>%
+              mutate(CU_Typ = fct_relabel(CU_Typ,
+                                          ~ paste(., "Cnt", sep = '_'))) %>%
+              spread(CU_Typ, cnt,
+                     fill = 0)) %>%
+  left_join(dash2018_cu %>%
+              st_drop_geometry() %>%
+              group_by(SiteNam, Hab_Rch) %>%
+              summarise_at(vars(Length,
+                                SHAPE_A,
+                                Wod_Cnt,
+                                N_Bllst,
+                                N_ChFrm,
+                                N_Wet,
+                                Wod_Vlm,
+                                Jam_Vlm,
+                                Undrc_C,
+                                Undrc_L,
+                                Undrc_A),
+                           list(sum))) %>%
+  left_join(dash2018_cu %>%
+              group_by(SiteNam, Hab_Rch) %>%
+              summarise(Pool_A = sum(SHAPE_A[CU_Typ == "Pool"]),
+                        n_SC = n_distinct(Sgmnt_ID[Sgmnt_ID > 1]),
+                        SC_A = sum(SHAPE_A[Sgmnt_ID > 1])) %>%
+              st_drop_geometry() %>%
+              as_tibble()) %>%
+  left_join(dash2018_cu %>%
+              group_by(SiteNam, Hab_Rch) %>%
+              summarise_at(vars(Mx_Dpth),
+                           lst(n_pool = ~ sum(!is.na(.)),
+                               Pool_Mx_Dpth = max, 
+                               Pool_Avg_Mx_Dpth = mean, 
+                               Pool_CV_Mx_Dpth = ~ sd(., na.rm = T) / mean(., na.rm = T)),
+                           na.rm = T) %>%
+              st_drop_geometry() %>%
+              as_tibble()) %>%
+  left_join(dash2018_cu %>%
+              # mutate(Tot_Cov = 100 - No_Cov) %>%
+              group_by(SiteNam, Hab_Rch) %>%
+              summarise_at(vars(Tot_Cov = Tot_Cov,
+                                Aq_Veg_Cov = Aq_Veg_Cov,
+                                d50 = d50,
+                                d84 = d84,
+                                Prc_Fns = Prc_Fns,
+                                Prc_Grv = Prc_Grv,
+                                Prc_Cbb = Prc_Cbb,
+                                Prc_Bld = Prc_Bld),
+                           list(~ weighted.mean(., 
+                                                w = SHAPE_A,
+                                                na.rm = T))) %>%
+              st_drop_geometry() %>%
+              mutate_at(vars(ends_with("Cov")),
+                        list(round),
+                        digits = 2) %>%
+              as_tibble()) %>%
+  ungroup() %>%
+  mutate(FstNT_Cnt = Run_Cnt,
+         FstTurb_Cnt = Riffle_Cnt + Rapid_Cnt,
+         Slowwater_Pct = (Pool_A / SHAPE_A) * 100,
+         SC_Pct = (SC_A / SHAPE_A) * 100,
+         LWFreq_Wet = (N_Wet / Length) * 100) %>%
+  mutate_at(vars(-SiteNam, -Hab_Rch),
+            ~replace(., . %in% c("NaN", "-Inf"), NA))
+
+# are these results the same?
+# dash2018_hr2 %>%
+dash2018_hr3 %>%
+  select(one_of(names(dash2018_hr))) %>%
+  st_drop_geometry() %>%
+  select(-CU_IDs) %>%
+  mutate_at(vars(which(sapply(., FUN = function(x) class(x)[1]) %in% c('numeric', 'integer'))),
+            list(round),
+            digits = 5) %>%
+  identical(dash2018_hr %>%
+              st_drop_geometry() %>%
+              select(-CU_IDs) %>%
+              mutate_at(vars(which(sapply(., FUN = function(x) class(x)[1]) %in% c('numeric', 'integer'))),
+                        list(round),
+                        digits = 5))
+
+
 
 # Others to consider?
 #Ssc_Avg_Wdth = weighted.mean(Avg_Wdth, Length) if(Off_chnl_Typ == "Ssc")
